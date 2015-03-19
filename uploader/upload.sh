3cbcd3cc4ca3c6cc19e0fcf99a57189675a36686
Kyle McDonald <kyle@kylemcdonald.net>
2015-03-19 18:18:59 +0000

    starting merge

---
+++ uploader/upload.sh
@@ -0,0 +1,8 @@
+youtube-upload \
+	--client-secrets=client_secret.json \
+	--privacy=unlisted \
+	--description="http://www.exhaustingacrowd.com/" \
+	--title="Exhausting a Crowd" \
+	--title-template="{title} ({n}/{total})" \
+	--location="latitude=40.687730, longitude=-73.979779, altitude=0" \
+	/Volumes/LaCie/$1/*.MP4
