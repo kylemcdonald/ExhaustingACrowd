98648fd94f6575b932d1f7aed798c390ba998330
Jonas Jongejan <jonas@halfdanj.dk>
2015-06-02 13:02:30 -0400

    Damn.... fixing posting error

--- typescript/Note.ts
+++ typescript/Note.ts
@@ -101,16 +101,18 @@ class Note {
             this.path = new Path(json.path);
 
 
-            var dur = this.time_end - this.time_begin;
-            var minDur = this.text.length / 140 * 6000; // 6 seconds to read 140 characters
-            if(dur < minDur){
-                var newPoint = new PathPoint(
-                    this.path.last().x,
-                    this.path.last().y,
-                    this.path.first().time + minDur
-                );
-                this.path.push(newPoint);
-                this.time_end = this.time_begin + minDur;
+            if(this.text) {
+                var dur = this.time_end - this.time_begin;
+                var minDur = this.text.length / 140 * 6000; // 6 seconds to read 140 characters
+                if (dur < minDur) {
+                    var newPoint = new PathPoint(
+                        this.path.last().x,
+                        this.path.last().y,
+                        this.path.first().time + minDur
+                    );
+                    this.path.push(newPoint);
+                    this.time_end = this.time_begin + minDur;
+                }
             }
         }
     }
