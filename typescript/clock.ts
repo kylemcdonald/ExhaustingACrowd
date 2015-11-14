8e1f16ba78881a6cb759a4a70a2eadbd0445c649
Jonas Jongejan <jonas@halfdanj.dk>
2015-11-14 15:09:34 +0100

    Check for 24hour time format

--- typescript/clock.ts
+++ typescript/clock.ts
@@ -29,14 +29,30 @@ class Clock {
 
         var hours = date.getHours();
         var minutes = date.getMinutes();
-        var ampm = hours >= 12 ? 'PM' : 'AM';
-        var minutesString = minutes < 10 ? '0' + String(minutes) : String(minutes);
-        hours = hours % 12;
-        var hoursString = hours < 1 ? '12' : String(hours);
+
+        var language = (navigator.language || navigator.browserLanguage).split('-')[0];
+        var minutesString, hoursString;
+        var dateString = date.toLocaleString(language);
+
+        if (dateString.match(/am|pm/i) || date.toString().match(/am|pm/i) )
+        {
+            //12 hour clock
+            var ampm = hours >= 12 ? 'PM' : 'AM';
+             minutesString = minutes < 10 ? '0' + String(minutes) : String(minutes);
+            minutesString += ' ' + ampm;
+            hours = hours % 12;
+             hoursString = hours < 1 ? '12' : String(hours);
+        }
+        else
+        {
+            //24 hour clock
+            minutesString = minutes < 10 ? '0' + String(minutes) : String(minutes);
+             hoursString =  String(hours);
+        }
 
         this.blink(this.colon);
         $('#hour').html(hoursString.replace(/0/g, 'O'));
-        $('#minute').html(minutesString.replace(/0/g, 'O') + ' ' + ampm);
+        $('#minute').html(minutesString.replace(/0/g, 'O') );
     }
 
 
