31d4b636d700fa2e04e31dce3ada7ee0a933b7e5
Kyle McDonald <kyle@kylemcdonald.net>
2015-12-19 14:32:55 -0500

    switched site back to london, added location selector at top left

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(raven.middleware.express('https://edf1ff6b26ca41b0a9bbb280902b8c4e:e709b
 
 
 app.get('/', function(req, res) {
-  res.redirect('/netherlands');
+  res.redirect('/london');
 });
 
 app.get('/netherlands', function(req, res) {
